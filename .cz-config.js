0113e2f1a74ed1027448054f277e8dc728991d5b
hrenx <hrenx_owen@163.com>
2022-11-15 14:25:03 +0800

    🧪Test(cz): 测试cz配置

--- .cz-config.js
+++ .cz-config.js
@@ -1,31 +1,25 @@
 "use strict"
 module.exports = {
   types: [
-    { value: "✨特性", name: "特性:    一个新的特性" },
-    { value: "🐛修复", name: "修复:    修复一个Bug" },
-    { value: "📝文档", name: "文档:    变更的只有文档" },
-    { value: "💄格式", name: "格式:    空格, 分号等格式修复" },
-    { value: "♻️重构", name: "重构:    代码重构，注意和特性、修复区分开" },
-    { value: "⚡️性能", name: "性能:    提升性能" },
-    { value: "✅测试", name: "测试:    添加一个测试" },
-    { value: "🔧工具", name: "工具:    开发工具变动(构建、脚手架工具等)" },
-    { value: "⏪回滚", name: "回滚:    代码回退" },
+    { value: "🌻Feat", name: "🌻 Feat 一个新的特性" },
+    { value: "🐞Fix", name: "🐞 Fix 修复一个Bug" },
+    { value: "🚀Performance", name: "🚀 Performance 性能优化" },
+    { value: "🚧Doing", name: "🚧 Doing 开发进行中" },
+    { value: "🔨Refactor", name: " 🔨 Refactor 代码重构, 注意和feat、fix区分开" },
+    { value: "⚠️ Delete", name: "⚠️  Delete 删除代码/文件" },
+    { value: "🧪Test", name: "🧪 Test 测试" },
+    { value: "📝Docs", name: "📝 Docs 变更的只有文档" },
+    { value: "⚙️ Config", name: "⚙️  Config 配置修改(如vite/eslint/prettier)" },
+    { value: "💩Shit", name: "💩 Shit 临时脏代码" },
+    { value: "🔀Merge", name: "🔀 Merge 合并代码" },
+    { value: "⏫Upgrade", name: "⏫ Upgrade 升级或新增依赖" },
+    { value: "⏬Downgrade", name: "⏬ Downgrade 降级或删除依赖" },
+    { value: "🔃Revert", name: "🔃 Revert 回滚代码" },
   ],
-  scopes: [{ name: "模块1" }, { name: "模块2" }, { name: "模块3" }, { name: "模块4" }],
-  // it needs to match the value for field type. Eg.: 'fix'
-  /*  scopeOverrides: {
-    fix: [
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },  */
-  // override the messages, defaults are as follows
+  // scopes: [{ name: "模块1" }, { name: "模块2" }, { name: "模块3" }, { name: "模块4" }],
   messages: {
     type: "选择一种你的提交类型:",
-    scope: "选择一个scope (可选):",
-    // used if allowCustomScopes is true
+    scope: "请输入作用范围:",
     customScope: "Denote the SCOPE of this change:",
     subject: "短说明:\n",
     body: '长说明，使用"|"换行(可选)：\n',
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,37 +1,34 @@
+// https://gitee.com/help/articles/4231#article-header1
 module.exports = {
-  // rules: {
-  //   // Header
-  //   "header-max-length": [2, "always", 200],
-  //   // <type>枚举
-  //   "type-enum": [2, "always", ["init", "feat", "fix", "ui", "refactor", "replace", "deploy", "docs", "test", "chore", "style", "revert", "add", "minus", "del"]],
-  //   // <type> 不能为空
-  //   "type-empty": [2, "never"],
-  //   // <type> 格式 小写
-  //   "type-case": [2, "always", "lower-case"],
-  //   // <scope> 不能为空
-  //   "scope-empty": [2, "never"],
-  //   // <scope> 格式 小写
-  //   "scope-case": [2, "always", "lower-case"],
-  //   // <subject> 不能为空
-  //   "subject-empty": [2, "never"],
-  //   // <subject> 以.为结束标志
-  //   "subject-full-stop": [2, "never", "."],
-  //   // <subject> 格式
-  //   // 可选值
-  //   // 'lower-case' 小写 lowercase
-  //   // 'upper-case' 大写 UPPERCASE
-  //   // 'camel-case' 小驼峰 camelCase
-  //   // 'kebab-case' 短横线 kebab-case
-  //   // 'pascal-case' 大驼峰 PascalCase
-  //   // 'sentence-case' 首字母大写 Sentence case
-  //   // 'snake-case' 下划线 snake_case
-  //   // 'start-case' 所有首字母大写 start-case
-  //   "subject-case": [2, "never", []],
-  //   // <body> 以空行开头
-  //   "body-leading-blank": [1, "always"],
-  //   // <footer> 以空行开头
-  //   "footer-leading-blank": [1, "always"],
-  // },
-  // extends: ["@commitlint/config-conventional", "cz"],
-  extends: ["cz"],
+  rules: {
+    // Header
+    "header-max-length": [2, "always", 72],
+    // <type>枚举
+    "type-enum": [
+      2,
+      "always",
+      ["🌻Feat", "🐞Fix", "🚀Performance", "🚧Doing", "🔨Refactor", "⚠️ Delete", "🧪Test", "📝Docs", "⚙️ Config", "💩Shit", "🔀Merge", "⏫Upgrade", "⏬Downgrade", "🔃Revert"],
+    ],
+    // <type> 不能为空
+    "type-empty": [2, "always"],
+    // <scope> 不能为空
+    "scope-empty": [2, "always"],
+    // <subject> 不能为空
+    "subject-empty": [2, "always"],
+    // 可选值
+    // 'lower-case' 小写 lowercase
+    // 'upper-case' 大写 UPPERCASE
+    // 'camel-case' 小驼峰 camelCase
+    // 'kebab-case' 短横线 kebab-case
+    // 'pascal-case' 大驼峰 PascalCase
+    // 'sentence-case' 首字母大写 Sentence case
+    // 'snake-case' 下划线 snake_case
+    // 'start-case' 所有首字母大写 start-case
+    // "subject-case": [2, "never", []],
+    // <body> 以空行开头
+    // "body-leading-blank": [1, "always"],
+    // <footer> 以空行开头
+    // "footer-leading-blank": [1, "always"],
+  },
+  extends: ["@commitlint/config-conventional", "cz"],
 }
