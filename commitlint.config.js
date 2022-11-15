0f3f9351d754bf8431e891b808641bf5990451fe
hrenx <hrenx_owen@163.com>
2022-11-15 17:46:02 +0800

    Config(commitlint): 增加extends和rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  extents: ["@commitlint/config-conventional"],
+  extends: ["@commitlint/config-conventional", "cz"],
   rules: {
     "type-enum": [2, "always", ["Feat", "Fix", "Performance", "Doing", "Refactor", "Delete", "Test", "Docs", "Config", "Shit", "Merge", "Upgrade", "Downgrade", "Revert"]],
     "header-max-length": [2, "never", 72],
     "type-empty": [2, "never"],
+    "type-case": [2, "always", "pascal-case"],
     "scope-empty": [2, "never"],
     "scope-max-length": [2, "never", 50],
     "subject-empty": [2, "never"],
