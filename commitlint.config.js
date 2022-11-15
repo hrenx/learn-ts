a391d469ea511721432564d367dba32ed0fa681c
hrenx <hrenx_owen@163.com>
2022-11-15 14:39:36 +0800

    🧪Test(commitlint): commitlint测试

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,34 +1,19 @@
-// https://gitee.com/help/articles/4231#article-header1
 module.exports = {
+  extents: ["@commitlint/config-conventional"],
   rules: {
-    // Header
-    "header-max-length": [2, "always", 72],
-    // <type>枚举
+    // "body-leading-blank": [1, "always"],
+    // "footer-leading-blank": [1, "always"],
+    // "header-max-length": [2, "always", 72],
+    // "scope-case": [2, "always", "lower-case"],
+    // "subject-case": [2, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
+    // "subject-empty": [2, "never"],
+    // "subject-full-stop": [2, "never", "."],
+    // "type-case": [2, "always", "lower-case"],
+    // "type-empty": [2, "never"],
     "type-enum": [
       2,
       "always",
       ["🌻Feat", "🐞Fix", "🚀Performance", "🚧Doing", "🔨Refactor", "⚠️ Delete", "🧪Test", "📝Docs", "⚙️ Config", "💩Shit", "🔀Merge", "⏫Upgrade", "⏬Downgrade", "🔃Revert"],
     ],
-    // <type> 不能为空
-    "type-empty": [2, "always"],
-    // <scope> 不能为空
-    "scope-empty": [2, "always"],
-    // <subject> 不能为空
-    "subject-empty": [2, "always"],
-    // 可选值
-    // 'lower-case' 小写 lowercase
-    // 'upper-case' 大写 UPPERCASE
-    // 'camel-case' 小驼峰 camelCase
-    // 'kebab-case' 短横线 kebab-case
-    // 'pascal-case' 大驼峰 PascalCase
-    // 'sentence-case' 首字母大写 Sentence case
-    // 'snake-case' 下划线 snake_case
-    // 'start-case' 所有首字母大写 start-case
-    // "subject-case": [2, "never", []],
-    // <body> 以空行开头
-    // "body-leading-blank": [1, "always"],
-    // <footer> 以空行开头
-    // "footer-leading-blank": [1, "always"],
   },
-  extends: ["@commitlint/config-conventional", "cz"],
 }
