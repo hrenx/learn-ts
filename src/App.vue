676bb04a84262060f1ee0997f1264632d16b1e24
hrenx <hrenx_owen@163.com>
2022-11-16 15:26:08 +0800

    Doing(view): page

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,6 @@
 
 <script setup lang="ts">
 import { RouterView } from "vue-router"
-
-console.log("123")
 </script>
 
 <!-- <style scoped></style> -->
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue"
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <el-input v-model="ceshi" placeholder="请输入"></el-input>
+    <el-button type="primary" @click="handleClick">测试</el-button>
+  </div>
 </template>
+<script lang="ts" setup>
+import { ref } from "vue"
+import { ElMessage } from "element-plus"
+
+const ceshi = ref("")
+
+function handleClick() {
+  ElMessage.error("测试")
+}
+</script>
