e39078f2c7264278879a1ec53d9c4bfd2bb87ed6
hrenx <hrenx_owen@163.com>
2022-11-16 15:22:13 +0800

    Config(ElementPlus): ElementPlus按需导入

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,26 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
+// ElementPlus
+import AutoImport from "unplugin-auto-import/vite"
+import Components from "unplugin-vue-components/vite"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import { createStyleImportPlugin, ElementPlusResolve } from "vite-plugin-style-import"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    createStyleImportPlugin({
+      resolves: [ElementPlusResolve()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
