3d5c642599c82e34f638eebe2e4edb18347e423f
hrenx <hrenx.owen@163.com>
2022-11-17 22:02:41 +0800

    🚧Doing(class Request): Request 封装

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import type { AxiosInstance } from "axios"
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios"
 import type { RequestConfig, Interceptors } from "./type"
 
 class Request {
@@ -10,10 +10,27 @@ class Request {
     this.instance = axios.create(config)
     this.interceptors = config.interceptors
 
+    // 实例的拦截器
     if (this.interceptors !== undefined) {
       this.instance.interceptors.request.use(this.interceptors?.requestInterceptor, this.interceptors?.requestInterceptorCatch)
       this.instance.interceptors.response.use(this.interceptors?.responseInterceptor, this.interceptors?.responseInterceptorCatch)
     }
+
+    // 类全局的拦截器
+    this.instance.interceptors.request.use((config: AxiosRequestConfig) => config)
+    this.instance.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      (err: any) => err
+    )
+  }
+
+  // 请求函数
+  async request(config: RequestConfig): Promise<AxiosResponse<any, any>> {
+    // 单个请求上的拦截器
+    if (config.interceptors?.requestInterceptor) config.interceptors.requestInterceptor(config)
+    const response: AxiosResponse = await this.instance.request(config)
+    if (config.interceptors?.responseInterceptor) config.interceptors.responseInterceptor(response)
+    return response
   }
 }
 
